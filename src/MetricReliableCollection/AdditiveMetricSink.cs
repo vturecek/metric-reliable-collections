16de3a8500cf86409aa11ee4853671bdc12aa68e
Vaclav Turecek <vaclav@live.com>
2016-03-29 11:17:31 -0700

    Simple implementation of additive metric sink.

--- src/MetricReliableCollection/AdditiveMetricSink.cs
+++ src/MetricReliableCollection/AdditiveMetricSink.cs
@@ -5,11 +5,14 @@
 namespace MetricReliableCollections
 {
     using Microsoft.ServiceFabric.Data;
+    using Microsoft.ServiceFabric.Data.Collections;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Fabric;
     using System.Threading.Tasks;
-
+    using Extensions;
+    using System.Threading;
     internal class AdditiveMetricSink : IMetricSink
     {
         private readonly IReliableStateManager stateManager;
@@ -21,19 +24,66 @@ namespace MetricReliableCollections
             this.metricStoreName = metricStoreName;
         }
 
-        internal Task<IEnumerable<LoadMetric>> SumMetricsAsync()
+        internal async Task<IEnumerable<LoadMetric>> SumMetricsAsync()
         {
-            throw new NotImplementedException();
-        }
-        
-        public Task ReportLoadAsync(ITransaction tx, Uri collectionName, IEnumerable<LoadMetric> metrics)
-        {
-            throw new NotImplementedException();
+            IReliableDictionary<string, List<LoadMetric>> metricDictionary =
+                await this.stateManager.GetOrAddAsync<IReliableDictionary<string, List<LoadMetric>>>(this.metricStoreName);
+
+            Dictionary<string, int> totals = new Dictionary<string, int>();
+
+            using (ITransaction tx = this.stateManager.CreateTransaction())
+            {
+                await metricDictionary.ForeachAsync(tx, CancellationToken.None, item =>
+                {
+                    foreach (LoadMetric metric in item.Value)
+                    {
+                        if (totals.ContainsKey(metric.Name))
+                        {
+                            totals[metric.Name] += metric.Value;
+                        }
+                        else
+                        {
+                            totals[metric.Name] = metric.Value;
+                        }
+                    }
+                });
+            }
+
+            return totals.Select(x => new LoadMetric(x.Key, x.Value));
         }
 
-        public Task AddLoadAsync(ITransaction tx, Uri collectionName, IEnumerable<LoadMetric> metrics)
+        public async Task ReportLoadAsync(ITransaction tx, Uri collectionName, IEnumerable<LoadMetric> metrics)
         {
-            throw new NotImplementedException();
+            IReliableDictionary<string, List<LoadMetric>> metricDictionary =
+                await this.stateManager.GetOrAddAsync<IReliableDictionary<string, List<LoadMetric>>>(this.metricStoreName);
+
+            await metricDictionary.SetAsync(tx, collectionName.ToString(), metrics.ToList());
+        }
+
+        public async Task AddLoadAsync(ITransaction tx, Uri collectionName, IEnumerable<LoadMetric> metrics)
+        {
+            IReliableDictionary<string, List<LoadMetric>> metricDictionary =
+                await this.stateManager.GetOrAddAsync<IReliableDictionary<string, List<LoadMetric>>>(this.metricStoreName);
+
+            await metricDictionary.AddOrUpdateAsync(tx, collectionName.ToString(), metrics.ToList(), (key, value) =>
+            {
+                List<LoadMetric> currentMetrics = new List<LoadMetric>();
+                foreach (LoadMetric newMetric in metrics)
+                {
+                    LoadMetric current = value.Find(x => String.Equals(x.Name, newMetric.Name, StringComparison.OrdinalIgnoreCase));
+
+                    if (current == null)
+                    {
+                        currentMetrics.Add(newMetric);
+                    }
+                    else
+                    {
+                        currentMetrics.Add(new LoadMetric(current.Name, current.Value + newMetric.Value));
+                    }
+                }
+
+                return currentMetrics;
+            });
         }
     }
 }
